495b9559d3b06ec8369c5900151e7d656090af13
MasterManiac980 <trainboy980@gmail.com>
2022-11-20 13:50:13 -0500

    added "<<" operator for Player class

--- player.cpp
+++ player.cpp
@@ -1,6 +1,7 @@
-#include "Player.h"
+#include "player.h"
 
 #include <string>
+#include <iostream>
 
 Player::Player(int32_t currentYear, std::string firstName, std::string lastName, int32_t yearOfBirth, bool paidStatus = false)
 {
@@ -10,4 +11,18 @@ Player::Player(int32_t currentYear, std::string firstName, std::string lastName,
 	m_paidStatus = paidStatus;
 
 	m_category = determineCategory(currentYear);
+}
+
+std::ostream &operator<<(std::ostream &out, Player &toPrint)
+{
+	out << toPrint.m_lastName << ", " << toPrint.m_firstName << " - " << toPrint.m_category << " ";
+	if (toPrint.m_paidStatus)
+	{
+		out << "Paid" << std::endl;
+	}
+	else
+	{
+		out << "Not Paid" << std::endl;
+	}
+	return out;
 }
--- player.h
+++ player.h
@@ -1,15 +1,20 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 
 class Player
 {
 public:
 	Player() = delete;
-	Player(int32_t currentYear, std::string firstName, std::string lastName, int32_t yearOfBirth, bool paidStatus = false);
+	Player(int32_t currentYear, std::string firstName, std::string lastName, int32_t yearOfBirth, bool paidStatus);
 
 	std::string getLast() { return m_lastName; }
 	std::string getFirst() { return m_firstName; }
+
+	void editPlayer();
+
+	friend std::ostream &operator<<(std::ostream &out, Player &toPrint);
 
 private:
 	std::string determineCategory(int32_t currentYear);

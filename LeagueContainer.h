696c69ec7acba1425aa91e871070d5c0c900bb63
carykees98 <trainboy980@gmail.com>
2022-11-27 00:56:43 -0500

    rearranged function declarations and updated return types

--- LeagueContainer.h
+++ LeagueContainer.h
@@ -5,17 +5,25 @@
 #include <map>
 #include <string>
 #include <list>
+#include <utility>
 
 class LeagueContainer
 {
 public:
-	std::list<Player> searchForPlayers(bool &foundMatches) const;
+	// Accessors
+	int currentYear() const { return m_currentYear; }
+
+	// Search Related
+	std::list<std::pair<std::string, Player>> searchForPlayers(bool &foundMatches) const;
+	void saveLeagueToFile();
+	void saveSearchToFile(std::list<std::pair<std::string, Player>> &searchResult);
+
+	// Uncategorized
 	void addPlayer();
 	void printStatistics();
 	void resetLeague() { m_leaguePlayers.clear(); }
 	void startNewSeason();
-	void saveLeagueToFile();
-	void saveSearchToFile(std::list<Player> &searchResult);
+	void update(std::list<std::pair<std::string, Player>> &searchResult);
 
 private:
 	std::map<std::string, Player> m_leaguePlayers;
